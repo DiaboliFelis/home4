12101495963104253042bbe6ca58a4c7503a6619
Vassiliy <I-IvanZakharov@yandex.ru>
2023-10-08 22:56:06 +0300

    Initial commit

---
+++ hello.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var x:= ReadString('Enter the language you speak:');
+  case x of
+    'Русский': print('Привет');
+    'English': print('Hi');
+    'Français': Print('Bonjour');
+  end;
+end.
